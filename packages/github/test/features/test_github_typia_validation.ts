40859a3295e7bd0dcad41d7f730c3807c31230cf
michael <7471919@naver.com>
2025-03-13 19:45:40 +0900

    chore: update typia 8.0.1

---
+++ packages/github/test/features/test_github_typia_validation.ts
@@ -0,0 +1,7 @@
+// import { GithubService } from "@wrtnlabs/connector-github";
+// import typia from "typia";
+
+// export const test_github_typia_validation = async () => {
+//   typia.llm.application<GithubService, "chatgpt">();
+//   console.log("hi");
+// };
