ad23f11e8d99d92a860e903e7b9f435edb31d535
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-04-11 22:17:47 +0100

    fix: mock internal module

--- deploy/list-connectors.ts
+++ deploy/list-connectors.ts
@@ -1,10 +1,43 @@
 import { glob } from "tinyglobby";
 import fs from "node:fs/promises";
 import { join } from "node:path";
+import Module from 'node:module';
 
 const ROOT_DIR = join(__dirname, "..");
 
 const IGNORE_PACKAGE_LIST = ["api", "backend", "shared"];
+
+// @ts-ignore
+const originalLoad = Module._load;
+
+// mocuk the module loading to prevent errors when a module is not found
+// @ts-ignore
+Module._load = function(path: string) {
+  // mock typia
+  if (path.includes("typia")) {
+    // prevent mocking the typia/lib/transform module
+    if(path.includes("typia/lib/transform")) {
+      return originalLoad.apply(this, arguments);
+    }
+    console.warn(`Warning: Mock typia module`);
+    return {
+      createIs: () => {}
+    };
+  }
+  try {
+    return originalLoad.apply(this, arguments);
+  } catch (error) {
+    // ignore integral package import errors
+    // @ts-ignore
+    if(path.includes("@wrtnlabs")) {
+      console.warn(`Warning: Module "${path}" not found. Using mock instead.`);
+      return {}
+    }
+    // mock typia
+    return originalLoad.apply(this, arguments);
+  }
+};
+
 
 async function main() {
   const packages = await glob(
