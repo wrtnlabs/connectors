296d8971b967f751b7960aa1d33c3dde935663e1
Michael <michael@wrtn.io>
2025-04-13 16:14:16 +0900

    Fix: modify the connector-list.json to add environment variables (#989)
    * chore: fix the list-connectors.ts
    
    * chore: remove console.log from list-connectors.ts
    
    * chore: remove catch from main function in list-connectors.ts
    
    * fix: run lint-connectors in transpile-only mode
    
    make it faster and prevent running typia transpiler
    
    * fix: mock internal module
    
    * chore: update connectors-list.json
    
    * fix: remove unused function from connectors-list.ts
    
    * refactor: use fs-extra / get version from package.json using import syntax etc
    
    * chore: fix typo and prettier
    
    * chore: update version of connectors-list.json
    
    * chore: add TODO comment in list-connectors.ts
    
    * chore: fix connectors-list.json
    
    * chore: add the build trigger of connectors-list.json to build workflow
    
    ---------
    
    Co-authored-by: ryoppippi <1560508+ryoppippi@users.noreply.github.com>

---
+++ deploy/list-connectors.ts
@@ -0,0 +1,84 @@
+import { glob } from "tinyglobby";
+import { join } from "node:path";
+import Module from "node:module";
+import fs from "fs-extra";
+import type { PackageJson, RequiredDeep } from "type-fest";
+import { version } from "../package.json";
+
+const ROOT_DIR = join(__dirname, "..");
+
+const IGNORE_PACKAGE_LIST = ["api", "backend", "shared"];
+
+// @ts-ignore
+const originalLoad = Module._load;
+
+// mock the module loading to prevent errors when a module is not found
+// @ts-ignore
+Module._load = function (path: string) {
+  // mock typia
+  if (path.includes("typia")) {
+    // prevent mocking the typia/lib/transform module
+    if (path.includes("typia/lib/transform")) {
+      return originalLoad.apply(this, arguments);
+    }
+    console.warn(`Warning: Mock typia module`);
+    return {
+      createIs: () => {},
+    };
+  }
+  try {
+    return originalLoad.apply(this, arguments);
+  } catch (error) {
+    // ignore integral package import errors
+    // @ts-ignore
+    if (path.includes("@wrtnlabs")) {
+      console.warn(`Warning: Module "${path}" not found. Using mock instead.`);
+      return {};
+    }
+    // mock typia
+    return originalLoad.apply(this, arguments);
+  }
+};
+
+async function main() {
+  const packages = await glob(
+    ["packages/*", ...IGNORE_PACKAGE_LIST.map((name) => `!packages/${name}`)],
+    {
+      onlyDirectories: true,
+      absolute: true,
+    },
+  );
+
+  /** extract package name from package.json */
+  const extractPackageInfo = async (packagePath: string) => {
+    const { ENV_LIST = [] }: { ENV_LIST: string[] } = await import(
+      join(packagePath, "src", "index.ts")
+    );
+
+    const { name } = (await fs.readJSON(
+      join(packagePath, "package.json"),
+      // @TODO: fix unsafe type assertion
+    )) as RequiredDeep<Pick<PackageJson, "name">>;
+    return { name, envList: ENV_LIST };
+  };
+
+  console.log("Packages");
+  console.log(packages);
+  const connectors = await Promise.all(packages.map(extractPackageInfo));
+
+  connectors.sort((a, b) => a.name.localeCompare(b.name));
+
+  const connectorsListData = {
+    connectors,
+    version,
+  };
+
+  /** write package list to file */
+  await fs.outputJson(
+    join(ROOT_DIR, "connectors-list.json"),
+    connectorsListData,
+    { spaces: 2 },
+  );
+}
+
+main();
